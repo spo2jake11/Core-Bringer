9bbb42109be57062d35c1a260e34ce8be6d7ad73
spo2jake11 <spo2jake11@gmail.com>
2025-07-08 09:40:35 +0800

    update

--- core/src/main/java/com/altf4studios/corebringer/entities/BattleEntity.java
+++ core/src/main/java/com/altf4studios/corebringer/entities/BattleEntity.java
@@ -1,6 +1,9 @@
 package com.altf4studios.corebringer.entities;
 
 public interface BattleEntity {
-    void takeDamage(int ammount);
+    void takeDamage(int amount);
     void applyStatus();
+    void target(Player player);
+    void target(Enemy enemy);
+
 }
--- core/src/main/java/com/altf4studios/corebringer/interpreter/JShellExecutor.java
+++ core/src/main/java/com/altf4studios/corebringer/interpreter/JShellExecutor.java
@@ -1,4 +1,12 @@
 package com.altf4studios.corebringer.interpreter;
+import jdk.jshell.JShell;
+import jdk.jshell.SnippetEvent;
 
 public class JShellExecutor {
+    private JShell shell;
+
+    public JShellExecutor(){
+        shell = JShell.create();
+        System.out.println(shell.eval("System.out.println('Hello World')"));
+    }
 }
