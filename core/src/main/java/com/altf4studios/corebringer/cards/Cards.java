504be5cb4abd89c534957b7ee82885637d53749b
RRnald <150567106+AtSush11@users.noreply.github.com>
2025-07-22 17:39:10 +0800

    feat: Implement card logic resolution and JShell integration

--- core/src/main/java/com/altf4studios/corebringer/cards/Cards.java
+++ core/src/main/java/com/altf4studios/corebringer/cards/Cards.java
@@ -1,6 +1,6 @@
 package com.altf4studios.corebringer.cards;
 
-import com.altf4studios.corebringer.effects.CardEffects;
+import com.altf4studios.corebringer.effects.CardEffectType;
 import com.altf4studios.corebringer.slots.SlotType;
 
 import java.util.List;
@@ -10,12 +10,18 @@ public class Cards {
     public String name;
     public String description;
     public int baseDamage;
+    public int baseBlock;
+    public int baseHeal;
+    public String statusName;
+    public int statusAmount;
+    public int energyAmount;
     public SlotType type;
     public Tags tags;
     public TargetType targetType;
     public int cost;
     public String codeEffect;
     public String suggestion;
+    public List<CardEffectType> effectTypes;
 
     public enum TargetType{
         SELF, ENEMY;
@@ -25,31 +31,32 @@ public class Cards {
         BUFF, DEBUFF, ATTACK, DEFENSE;
     }
 
-    public List<CardEffects.CardEffectType> Effects;
+    // TODO: This CardUI seems to be using an older implementation of effects.
+    // public List<CardEffects.CardEffectType> Effects;
 }
 
-    class CardUI {
-    public void renderCard(Cards card) {
-        if (card.Effects.contains(CardEffects.CardEffectType.INCREASE_ATK)) {
-            drawEffectIcon("");
-        }
-        if (card.Effects.contains(CardEffects.CardEffectType.APPLY_STATUS)){
-
-        }
-        if (card.Effects.contains(CardEffects.CardEffectType.MULTIHIT)){
-
-        }
-        if (card.Effects.contains(CardEffects.CardEffectType.ATTACK)){
-
-        }
-        if (card.Effects.contains(CardEffects.CardEffectType.DEFENSE)){
-
-        }
-        // ... Cost inclusion shall be added through development
-    }
-
-    private void drawEffectIcon(String iconName) {
-        // Drawing logic here
-    }
-}
+    //class CardUI {
+    //public void renderCard(Cards card) {
+    //    if (card.Effects.contains(CardEffects.CardEffectType.INCREASE_ATK)) {
+    //        drawEffectIcon("");
+    //    }
+    //    if (card.Effects.contains(CardEffects.CardEffectType.APPLY_STATUS)){
+    //
+    //    }
+    //    if (card.Effects.contains(CardEffects.CardEffectType.MULTIHIT)){
+    //
+    //    }
+    //    if (card.Effects.contains(CardEffects.CardEffectType.ATTACK)){
+    //
+    //    }
+    //    if (card.Effects.contains(CardEffects.CardEffectType.DEFENSE)){
+    //
+    //    }
+    //    // ... Cost inclusion shall be added through development
+    //}
+    //
+    //private void drawEffectIcon(String iconName) {
+    //    // Drawing logic here
+    //}
+//}
 
